35b50d2c268a4a1b94efa2a7b9f78c6045578d9a
ymoch <ymoch.dev@gmail.com>
2019-04-21 18:23:53 +0900

    Make effects installable.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Using [Python venv](https://docs.python.org/3/library/venv.html) is strongly rec
 
 ```sh
 pip install "fabric2>=2.4,<3"
+pip install "patchwork>=1.0,<2"
 pip install cryptography==2.4.2  # To reduce warnings.
 
 fab2 -H user@host provision
--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,7 @@
+import os
+
 from invoke import task
+from patchwork.transfers import rsync
 
 
 @task
@@ -27,5 +30,21 @@ def install_cmake(c):
 
 
 @task
+def install_pedal_pi_effects(c):
+    local_dir_path = os.path.dirname(__file__)
+    dir_name = 'pedal-pi-effects'
+
+    c.run(f'rm -rf {dir_name}')
+    rsync(c, f'{local_dir_path}/', f'{dir_name}/',
+        exclude=['.git'], rsync_opts='-C --filter=":- .gitignore"')
+    with c.cd(dir_name):
+        c.run('cmake .')
+        c.run('make')
+
+    c.sudo(f'bash -c \'cd "{dir_name}" && make install\'')
+
+
+@task
 def provision(c):
     install_bcm2835(c)
+    install_cmake(c)
