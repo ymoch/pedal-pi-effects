3da47e64f672fc25763138352d00e4566bcd153e
ymoch <ymoch.dev@gmail.com>
2019-04-27 02:31:50 +0900

    Add soft clipping.

--- src/ymoch/pedalpieffects/main.cc
+++ src/ymoch/pedalpieffects/main.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdint>
 #include <iostream>
@@ -9,6 +10,32 @@ using std::cerr;
 using std::endl;
 
 namespace {
+
+template <typename T>
+class SignalNormalizer {
+ public:
+  SignalNormalizer(const T& lower_limit, const T& upper_limit)
+      : lower_limit_(lower_limit),
+        upper_limit_(upper_limit),
+        width_(upper_limit_ - lower_limit_ + 1),
+        offset_(width_ / 2.0 + lower_limit) {}
+
+  double Normalize(const T& signal) const {
+    return (static_cast<double>(signal) - offset_) / width_;
+  }
+
+  double Unnormalize(double signal) const {
+    const auto unnormalized = signal * width_ + offset_;
+    return std::max(std::min(static_cast<T>(unnormalized), upper_limit_),
+                    lower_limit_);
+  }
+
+ private:
+  T lower_limit_;
+  T upper_limit_;
+  double width_;
+  double offset_;
+};
 
 template <uint32_t base, uint32_t exponent>
 struct Power {
@@ -29,7 +56,7 @@ constexpr uint32_t kPwmRange = Power<2, kNumBit>::value - 1;
 
 constexpr uint32_t kSpiClockDivider = BCM2835_SPI_CLOCK_DIVIDER_64;
 
-constexpr uint32_t kClockFrequencyHz = 169250; // Tuned manually.
+constexpr uint32_t kClockFrequencyHz = 169250;  // Tuned manually.
 
 constexpr uint8_t kPinPush1 = RPI_GPIO_P1_08;
 constexpr uint8_t kPinPush2 = RPI_V2_GPIO_P1_38;
@@ -38,7 +65,7 @@ constexpr uint8_t kPinFootSwitch1 = RPI_GPIO_P1_10;
 constexpr uint8_t kPinLed1 = RPI_V2_GPIO_P1_36;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   if (!bcm2835_init()) {
     cerr << "bcm2835_init failed. Are you running as root??" << endl;
     return 1;
@@ -61,11 +88,11 @@ int main(int argc, char **argv) {
   bcm2835_pwm_set_range(kPwmChannel1, kPwmRange);
 
   // define SPI bus configuration
-  bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);    // default
-  bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                 // default
+  bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);  // default
+  bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);               // default
   bcm2835_spi_setClockDivider(kSpiClockDivider);
-  bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                    // default
-  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);    // default
+  bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                  // default
+  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);  // default
 
   char mosi[10] = {0x01, 0x00, 0x00};  // 12 bit ADC read 0x08 ch0, - 0c for ch1
   char miso[10] = {0};
@@ -84,7 +111,9 @@ int main(int argc, char **argv) {
   bcm2835_gpio_set_pud(kPinFootSwitch1, BCM2835_GPIO_PUD_UP);
 
   // Main Loop
-  double amplification = 1.0;
+  const SignalNormalizer<uint32_t> normalizer(0, Power<2, 12>::value - 1);
+  double amplification = 1.5;
+  bool boosted = false;
   for (uint32_t read_timer = 0;; ++read_timer) {
     // read 12 bits ADC
     bcm2835_spi_transfernb(mosi, miso, 3);
@@ -93,34 +122,43 @@ int main(int argc, char **argv) {
     // Read the controls every 50000 times (0.25s) to save resources.
     constexpr uint32_t kControlCheckInterval = kClockFrequencyHz * 0.25;
     if (read_timer % kControlCheckInterval == 0) {
+      constexpr double kAmplificationDelta = 1.5;
+
       uint8_t push_1 = bcm2835_gpio_lev(kPinPush1);
-      uint8_t push_2 = bcm2835_gpio_lev(kPinPush2);
-      // uint8_t toggle_switch_1 = bcm2835_gpio_lev(kPinToggleSwitch1);
-      uint8_t foot_switch_1 = bcm2835_gpio_lev(kPinFootSwitch1);
-
-      // light the effect when foot switch 1 is activated.
-      bcm2835_gpio_write(kPinLed1, !foot_switch_1);
-
-      constexpr double kAmplificationDelta = 1.2;
       if (!push_1) {
         bcm2835_delay(100);  // 100ms delay for buttons debouncing.
         amplification /= kAmplificationDelta;
         cout << "Amplification: " << amplification << endl;
       }
+
+      uint8_t push_2 = bcm2835_gpio_lev(kPinPush2);
       if (!push_2) {
         bcm2835_delay(100);  // 100ms delay for buttons debouncing.
         amplification *= kAmplificationDelta;
         cout << "Amplification: " << amplification << endl;
       }
+
+      uint8_t toggle_switch_1 = bcm2835_gpio_lev(kPinToggleSwitch1);
+      boosted = !toggle_switch_1;
+
+      // light the effect when foot switch 1 is activated.
+      uint8_t foot_switch_1 = bcm2835_gpio_lev(kPinFootSwitch1);
+      bcm2835_gpio_write(kPinLed1, !foot_switch_1);
     }
 
-    double normalized_signal =
-        (static_cast<double>(input_signal) - 2048.0) / 2048.0;
-    double amplified_signal = normalized_signal * amplification;
-    uint32_t output_signal =
-        std::max(std::min(amplified_signal * 2048.0 + 2048.0, 4095.0), 0.0);
+    double normalized_signal = normalizer.Normalize(input_signal);
+
+    // Soft clipping.
+    double amplified_signal = std::tanh(normalized_signal * amplification) / 1.5;
+    if (amplified_signal < 0) {
+      amplified_signal *= 0.8;
+    }
+    if (!boosted) {
+      amplified_signal *= 0.7;
+    }
 
     // generate output PWM signal 6 bits
+    uint32_t output_signal = normalizer.Unnormalize(amplified_signal);
     bcm2835_pwm_set_data(1, output_signal & 0x3F);
     bcm2835_pwm_set_data(0, output_signal >> 6);
   }
